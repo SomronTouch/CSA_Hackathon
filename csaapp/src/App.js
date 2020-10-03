a805ae4c744485655411bbe5dff2b320995c9751
somron <somrontouche@gmail.com>
2020-10-03 12:40:20 -0400

    added a map visualizer

--- csaapp/src/App.js
+++ csaapp/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+import am4geodata_canadaHigh from "@amcharts/amcharts4-geodata/canadaHigh";
+
+
+// default react
+class App extends Component {
+  //  is used to create the chart.
+  componentDidMount() {
+    var chart = am4core.create("chartdiv", am4maps.MapChart);
+
+    // Set map definition
+    chart.geodata = am4geodata_canadaHigh;
+    
+    // Set projection
+    chart.projection = new am4maps.projections.Miller();
+    // Create map polygon series
+    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.useGeodata = true;
+    
+    // Configure series
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = am4core.color("#74B266");
+  }
+
+  // is used to cleanup the chart when it's done being used.
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
+  //  is used to create the <div> which the chart will be displayed inside of.
+  render() {
+    return (
+      <div id="chartdiv" style={{ width: "50%", height: "200%" }}></div>
+    );
+  }
 }
+
 
 export default App;
