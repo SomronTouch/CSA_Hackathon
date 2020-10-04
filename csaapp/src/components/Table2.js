3c44b44e6a56a27552544f088a377ce598f0c0be
Justice Ayela <justiceayela@cmail.carleton.ca>
2020-10-04 15:48:43 -0400

    risk region table

--- csaapp/src/components/Table2.js
+++ csaapp/src/components/Table2.js
@@ -3,10 +3,17 @@ import MaterialTable from 'material-table'
 
 export const Table2 =() => {
     const data =[
-        {region: "Ontario", fires: 1},
-        {region: "British Columbia", fires: 1},
-        {region: "Vancouver", fires: 1},
-        {region: "Calgary", fires: 1}
+        {region: "Northwest Territories", fires: "1.7e+10 m^2", risk: "high"},
+        {region: "Nunavut", fires: "1.58e+10 m^2", risk: "high"},
+        {region: "Yukon", fires: "2.11e+10", risk: "very high"},
+        {region: "British Columbia", fires: "1.06e+9", risk: "low"},
+        {region: "Alberta", fires: "3.64e+8 m^2", risk: "low"},
+        {region: "Saskatchewan", fires: "5.6e+7 m^2", risk: "low"},
+        {region: "Manitoba", fires: "2.45e+8", risk: "low"},
+        {region: "Ontario", fires: "0", risk: "no risk"},
+        {region: "Quebec", fires: "0", risk: "no risk"},
+        {region: "Nova Scotia", fires: "0", risk: "no risk"},
+        {region: "Prince Edward Island", fires: "0", risk: "no risk"},
     ]
 
     const columns =[
@@ -14,7 +21,10 @@ export const Table2 =() => {
             title: "Region", field: "region"
         },
         {
-            title: "Number of Active Wildfires", field: "fires"
+            title: "Square metre fire coverage", field: "fires"
+        },
+        {
+            title: "Risk of Fire", field: "risk"
         }
     ]
     return(<div>
